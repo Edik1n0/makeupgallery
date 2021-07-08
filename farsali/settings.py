45d916af870d7d3866e0a26cf6d9fed8074b4252
valentinc94 <valentincc94m@gmail.com>
2021-07-08 16:46:19 -0500

    Config setting 2

--- farsali/settings.py
+++ farsali/settings.py
@@ -106,7 +106,6 @@ WSGI_APPLICATION = "farsali.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-import dj_database_url
 
 DATABASES = {
     "default": {
@@ -115,6 +114,7 @@ DATABASES = {
     }
 }
 
+import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
